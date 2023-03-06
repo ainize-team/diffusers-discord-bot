2531a1539e7ce60c6f882885d0cf3e799e21f6bd
YoungJae Kim <byeongal@comcom.ai>
2023-03-06 14:05:06 +0900

    applicationGuildCommands

--- tools/deploy-commands.js
+++ tools/deploy-commands.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { REST, Routes } = require("discord.js");
 
-const { DISCORD_TOKEN, APPLICATION_ID } = process.env;
+const { DISCORD_TOKEN, APPLICATION_ID, GUILD_ID } = process.env;
 const commandHandlers = require("../dist/commands");
 
 const commands = Object.values(commandHandlers).map(command => command.getCommand().data.toJSON());
@@ -14,7 +14,7 @@ const rest = new REST({ version: "10" }).setToken(DISCORD_TOKEN);
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationCommands(APPLICATION_ID), { body: commands });
+    const data = await rest.put(Routes.applicationGuildCommands(APPLICATION_ID, GUILD_ID), { body: commands });
 
     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (error) {
